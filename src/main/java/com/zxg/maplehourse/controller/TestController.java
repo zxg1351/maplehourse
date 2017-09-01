71cd349f1c09d8f02ff325c5cc24f886c325ce7c
zxg1351 <zxg_546789@163.com>
2017-09-01 17:04:45 +0800

    20170831

--- src/main/java/com/zxg/maplehourse/controller/TestController.java
+++ src/main/java/com/zxg/maplehourse/controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,18 +26,26 @@ public class TestController {
     @Autowired
     private MCityRepository mProvinceRepository;
 
-
+    //@RequestParam(value = "pageNo") int pageNo
     @RequestMapping(value = "/select")
-    private Page<MCityModel> selectP(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                     @RequestParam(value = "size", defaultValue = "15") Integer size) {
+    private ModelAndView selectP(@PageableDefault Pageable pageNo) {
 
-
+        int page = 0;
         Sort sort = new Sort(Sort.Direction.DESC, "id");
-        Pageable pageable = new PageRequest(page, size, sort);
+//        if (pageNo.getPageNumber() < page) {
+//
+//            pageNo.getPageNumber() = page;
+//        }
+//        Pageable pageable = new PageRequest(pageNo, 10);
 
+        Page<MCityModel> mCityModels = mProvinceRepository.findAll(pageNo);
 
-        Page<MCityModel> mCityModels = mProvinceRepository.findAll(pageable);
-        return mCityModels;
+        ModelAndView modelAndView = new ModelAndView("/city");
+        modelAndView.addObject("citylist", mCityModels.getContent());
+        modelAndView.addObject("totalPageNumber", mCityModels.getTotalElements());
+        modelAndView.addObject("pageSize", mCityModels.getTotalPages());
+        modelAndView.addObject("number", mCityModels.getNumber());
+        return modelAndView;
 
     }
 
